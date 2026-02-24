53ddbaf12948e81e880e7c5d95e453306c87114f
Ibukunoluwa Fodunrin <ibukunfod@gmail.com>
2026-02-24 13:56:31 +0000

    footer gap fix

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 export default function Home() {
   return (
-    <div>
+    <div style={{ backgroundColor: "#000000" }}>
+      {" "}
       <div
         style={{
           height: "100vh",
--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -246,7 +246,7 @@ export default function Navbar() {
             initial={{ opacity: 0, y: -16 }}
             animate={{ opacity: 1, y: 0 }}
             exit={{ opacity: 0, y: -16 }}
-            transition={{ duration: 0.2, ease: "easeOut" }}
+            transition={{ duration: 0.4, ease: [0.32, 0.72, 0, 1] }}
             className="hidden lg:block fixed left-0 w-full backdrop-blur-md pointer-events-auto"
             style={{
               top: "120px",
